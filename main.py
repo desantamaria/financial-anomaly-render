e66cc3c5fa4d0a57672e92f0d90cf3c59babb125
Daniel Santamaria <santamaria.daniel316@gmail.com>
2025-01-17 18:40:18 -0800

    Add yfinance Fetch Function

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import pickle
 import pandas as pd
 import uvicorn
 import numpy as np
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import yfinance as yf
+
+
 
 # Initialize FastAPI
 app = FastAPI()
@@ -64,6 +69,43 @@ async def predict(data: dict):
         "prediction": prediction,
         "probability": probabilities
     }
+
+
+def createMA(df, window, ticker):
+    df[f'{ticker}_MA'] = df[ticker].rolling(window=window).mean()
+    return df
+
+@app.get("/getMarketData")
+async def get_market_data():
+    try:
+        ticker_list = ['VIX', 'DXY', 'JPY', 'GBP', 'MXEU', 'MXCN']
+        yf_tickers = ['^VIX', 'DX-Y.NYB', 'JPY=X', 'GBPUSD=X', 'MXEU.L', 'MXCN.SW']
+        
+        df = yf.download(yf_tickers, period='6mo')
+        column_mapping = dict(zip(yf_tickers, ticker_list))
+        df = df.rename(columns=column_mapping)
+        df = df[['Close']].droplevel(level=0, axis=1)
+        df = df.dropna()
+        
+        for ticker in ticker_list:
+            df = createMA(df, 12, ticker)
+        
+        df = df.dropna()
+        df = df.reset_index()
+        
+        json_data = {
+            ticker: [{'date': str(date), 'price': float(price), 'MA': float(ma)} 
+                    for date, price, ma in zip(df['Date'], df[ticker], df[f'{ticker}_MA'])]
+            for ticker in ticker_list
+        }
+        
+        dates = {
+            'dates': [str(date) for date in df['Date']]
+        }
+        
+        return {**json_data, **dates}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     
 # Run the app
 if __name__ == "__main__":
